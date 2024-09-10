3336fcd428c2231d4dba03038da80845efb89dea
ruizgomez250 <ruizgomez250@gmail.com>
2024-09-10 10:25:00 -0400

    mensaje

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,34 @@
 @extends('adminlte::page')
 
-
-
 @section('content_header')
-    <h1 class="m-0 text-dark">Dashboard</h1>
 @stop
 
 @section('content')
     <div class="row">
         <div class="col-12">
             <div class="card">
+                <div class="card-header bg-info text-white">
+                    <h5 class="mb-0">Acerca de: Sistema de Trazabilidad Documental</h5>
+                </div>
                 <div class="card-body">
-                    <p class="mb-0">Bienvenido al sistema de EasyStock!</p>
+                    
+                    <h6 class="font-weight-bold">Período Legislativo 2023 - 2024</h6>
+                    <p class="bg-light p-2 rounded">Dr. Raúl Luis Latorre Martínez</p>
+                    <p class="bg-light p-2 rounded">Presidente</p>
+                    
+                    <h6 class="font-weight-bold">Solicitud de Proyecto</h6>
+                    <p class="bg-light p-2 rounded">Secretaria General Administrativa</p>
+                    <p class="bg-light p-2 rounded">Secretario Administrativo: Abg. Julio Cesar Cabrera Guerrero</p>
+                    
+                    <h6 class="font-weight-bold">Construcción del Proyecto</h6>
+                    <p class="bg-light p-2 rounded">Dirección de Informática</p>
+                    <p class="bg-light p-2 rounded">Director: Nahuel Ojeda</p>
+                    <p class="font-weight-bold">Desarrolladores:</p> 
+                    <p class="bg-light p-2 rounded">Prog. Jorge Ruíz Gómez</p>
+                    <p class="bg-light p-2 rounded">Ing. Narciso Ramon Añazco</p>
+                    
+                    <h6 class="font-weight-bold">Capacitación</h6>
+                    <p class="bg-light p-2 rounded">Lic. Laura Susana Martínez</p>
                 </div>
             </div>
         </div>
